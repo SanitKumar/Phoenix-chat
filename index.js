89d996fd1d535cd8269ae6b2751b8d50915b52db
Sanit Kumar <106884165+SanitKumar@users.noreply.github.com>
2023-05-05 10:14:56 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,414 @@
+var data = {
+  chatinit: {
+    title: [
+      "Hello <span class='emoji'> &#128075;</span>",
+      "I am Mr. Chatbot",
+      "How can I help you?",
+    ],
+    options: [
+      "CSE Subjects Tutorials",
+      "Practice DSA Problems",
+      "Shopping <span class='emoji'> &#128090;</span>",
+      "News <span class='emoji'> &#128240;</span>",
+      "Music <span class='emoji'> &#127925;</span>",
+      "Movies <span class='emoji'> &#128250;</span>",
+    ],
+  },
+
+
+  cse: {
+    title: [
+      "These are the some CSE Subjects tutorials available free on Youtube......"
+    ],
+    options: [
+      "DSA with C++",
+      "DBMS",
+      "Operating System",
+      "Computer Networks",
+      "Theory of Computation",
+      "Design and Analysis of Algorithms",
+      "Compiler Design",
+      "Java Programming",
+    ],
+    url: {
+      more: "https://www.youtube.com/@GateSmashers/playlists",
+      link: [
+        "https://www.youtube.com/playlist?list=PLDzeHZWIZsTryvtXdMr6rPh4IDexB5NIA",
+        "https://www.youtube.com/playlist?list=PLxCzCOWd7aiFAN6I8CuViBuCdJgiOkT2Y",
+        "https://www.youtube.com/playlist?list=PLxCzCOWd7aiGz9donHRrE9I3Mwn6XdP8p",
+        "https://www.youtube.com/playlist?list=PLBlnK6fEyqRgMCUAG0XRw78UA8qnv6jEx",
+        "https://www.youtube.com/playlist?list=PLBlnK6fEyqRgp46KUv4ZY69yXmpwKOIev",
+        "https://www.youtube.com/playlist?list=PL-JvKqQx2Atd--1Gs3WB8nmWOWRbEM7WW",
+        "https://www.youtube.com/playlist?list=PLxCzCOWd7aiEKtKSIHYusizkESC42diyc",
+        "https://www.youtube.com/playlist?list=PLu0W_9lII9agS67Uits0UnJyrYiXhDS6q",
+      ],
+    },
+  },
+
+
+  practice: {
+    title: [
+      "These are some platforms where you can practice DSA problems......"
+    ],
+    options: [
+      "LeetCode",
+      "CodeChef",
+      "HackerRank",
+      "HackerEarth",
+      "InterviewBit",
+      "CodingNinja",
+      "Coderbyte",
+    ],
+    url: {
+      more: "https://practice.geeksforgeeks.org/explore?page=1&curated[]=1&sortBy=submissions&curated_names[]=SDE%20Sheet",
+      link: [
+        "https://leetcode.com/problemset/all/",
+        "https://www.codechef.com/practice",
+        "https://www.hackerrank.com/domains/algorithms?filters%5Bsubdomains%5D%5B%5D=dynamic-programming",
+        "https://www.hackerearth.com/practice/problems/?limit=20&offset=0",
+        "https://www.interviewbit.com/coding-interview-questions/",
+        "https://www.codingninjas.com/codestudio/problems",
+        "https://coderbyte.com/challenges",
+      ],
+    },
+  },
+
+
+  movies: {
+    title: [
+      "These are the some best movies streaming services available <span class='emoji'> &#128250;</span>"
+    ],
+    options: [
+      "Netflix",
+      "Amazon Prime",
+      "Voot",
+      "Zee5",
+      "Eros Now",
+      "Sony Liv",
+      "Jio Cinema",
+      "Airtel Xstream",
+      "Apple Tv+",
+      "Hotstar",
+    ],
+    url: {
+      more: "https://www.youtube.com/feed/storefront?bp=ogUCKAI%3D",
+      link: [
+        "https://www.netflix.com/in/",
+        "https://www.primevideo.com/",
+        "https://www.voot.com/movies",
+        "https://www.zee5.com/movies",
+        "https://erosnow.com/movies",
+        "https://www.sonyliv.com/custompage/movies-399",
+        "https://www.jiocinema.com/movies",
+        "https://www.airtelxstream.in/movies",
+        "https://tv.apple.com/",
+        "https://www.hotstar.com/in",
+      ],
+    },
+  },
+
+
+  music: {
+    title: [
+      "These are the some best music streaming services available <span class='emoji'> &#127925;</span>",
+    ],
+    options: [
+      "Spotify",
+      "Gaana",
+      "Jio Saavn",
+      "Wynk Music",
+      "Apple Music",
+      "Sound Cloud",
+      "Amazon Prime Music",
+      "Hungama Music",
+    ],
+    url: {
+      more: "https://www.youtube.com/channel/UC-9-kyTW8ZkZNDHQJ6FgpwQ",
+      link: [
+        "https://open.spotify.com/",
+        "https://gaana.com/",
+        "https://www.jiosaavn.com/",
+        "https://wynk.in/music",
+        "https://music.apple.com/us/browse",
+        "https://soundcloud.com/",
+        "https://music.amazon.in/",
+        "https://www.hungama.com/",
+      ],
+    },
+  },
+
+
+  news: {
+    title: ["Some famous news channels are.."],
+    options: [
+      "The Times of India",
+      "The Hindu",
+      "Hindustan Times",
+      "The Indian Express",
+      "Dainik Bhaskar",
+      "BBC News",
+      "Fox News",
+      "Al Jazeera",
+      "NDTV News",
+      "Global Times",
+      "Euro News",
+      "CNBC News",
+    ],
+    url: {
+      more: "https://news.google.com/home?hl=en-IN&gl=IN&ceid=IN:en",
+      link: [
+        "https://timesofindia.indiatimes.com/?from=mdr",
+        "https://www.thehindu.com/",
+        "https://www.hindustantimes.com/",
+        "https://indianexpress.com/",
+        "https://www.bhaskar.com/",
+        "https://www.bbc.com/",
+        "https://www.foxnews.com/",
+        "https://www.aljazeera.com/",
+        "https://www.ndtv.com/",
+        "https://www.globaltimes.cn/",
+        "https://www.euronews.com/",
+        "https://www.cnbc.com/world/?region=world",
+      ],
+    },
+  },
+
+
+  shopping: {
+    title: [
+      "Thanks for your response",
+      "Welcome to shopping zone <span class='emoji'> &#128090;</span>",
+      "Please select one of the below options to proceed further",
+    ],
+    options: [
+      "Electronics",
+      "Beauty Products",
+      "Mobiles",
+      "Men Fashion",
+      "Women fashion",
+    ],
+    url: {},
+  },
+  electronics: {
+    title: [
+      "Thanks for your response",
+      "Here are some electronic items for you",
+      "Click on it to know more",
+    ],
+    options: [
+      "Televisions",
+      "Cameras",
+      "Gaming Consoles",
+      "Headphones",
+      "Speakers",
+    ],
+    url: {
+      more: "https://www.flipkart.com/flipkart-electronics-new-store",
+      link: [
+        "https://www.flipkart.com/television-store",
+        "https://www.flipkart.com/camera-clp-store?otracker=nmenu_sub_Electronics_0_Camera",
+        "https://www.flipkart.com/gaming/gaming-consoles/pr?sid=4rr%2Cx1m",
+        "https://www.flipkart.com/headphones-store",
+        "https://www.flipkart.com/speakers/pr?sid=0o7",
+      ],
+    },
+  },
+  beauty: {
+    title: [
+      "Thanks for your response",
+      "Here are some beauty products for you",
+      "Click on it to know more",
+    ],
+    options: ["Make-up & Nails", "Skin Care", "Fragrance", "Hair care"],
+    url: {
+      more: "https://www.flipkart.com/beauty-and-grooming/pr?sid=g9b",
+      link: [
+        "https://www.flipkart.com/beauty-and-grooming/makeup/pr?sid=g9b%2Cffi",
+        "https://www.flipkart.com/beauty-and-grooming/body-face-skin-care/body-and-face-care/pr?sid=g9b%2Cema%2C5la",
+        "https://www.flipkart.com/beauty-and-grooming/fragrances/perfume/pr?sid=g9b%2C0yh%2Cjhz",
+        "https://www.flipkart.com/beauty-and-grooming/hair-care-and-accessory/hair-care/pr?sid=g9b%2Clcf%2Cqqm",
+      ],
+    },
+  },
+  mobiles: {
+    title: [
+      "Thanks for your response",
+      "These are some results based on your input",
+      "Click on it to know more",
+    ],
+    options: ["Mobile Phones", "Cases & Covers", "Power Banks", "Tablets"],
+    url: {
+      more: "https://www.flipkart.com/mobile-phones-store",
+      link: [
+        "https://www.flipkart.com/mobile-phones-store",
+        "https://www.flipkart.com/mobile-accessories/cases-and-covers/pr?sid=tyy,4mr,q2u",
+        "https://www.flipkart.com/mobile-accessories/power-banks/pr?sid=tyy%2C4mr%2Cfu6",
+        "https://www.flipkart.com/tablets/pr?sid=tyy%2Chry",
+      ],
+    },
+  },
+  men: {
+    title: [
+      "Thanks for your response",
+      "These are some results based on your input",
+      "Click on it to know more",
+    ],
+    options: ["Clothing", "Shirts", "T-shirts", "Jeans"],
+    url: {
+      more: "https://www.flipkart.com/q/mens-clothing",
+      link: [
+        "https://www.flipkart.com/q/mens-clothing",
+        "https://www.flipkart.com/mens-shirts/pr?sid=clo,ash,axc,mmk",
+        "https://www.flipkart.com/mens-tshirts/pr?sid=clo%2Cash%2Cank%2Cedy",
+        "https://www.flipkart.com/mens-jeans/pr?sid=clo,vua,k58,i51",
+      ],
+    },
+  },
+  women: {
+    title: [
+      "Thanks for your response",
+      "These are some results based on your input",
+      "Click on it to know more",
+    ],
+    options: ["Clothing", "Western Wear", "Ethnic Wear", "Footwear"],
+    url: {
+      more: "https://www.flipkart.com/q/womens-clothing",
+      link: [
+        "https://www.flipkart.com/q/womens-clothing",
+        "https://www.flipkart.com/q/western-dresses",
+        "https://www.flipkart.com/q/womens-ethnic-wear",
+        "https://www.flipkart.com/womens-footwear/pr?sid=osp,iko",
+      ],
+    },
+  },
+};
+
+document.getElementById("init").addEventListener("click", showChatBot);
+var cbot = document.getElementById("chat-box");
+
+var len1 = data.chatinit.title.length;
+
+function showChatBot() {
+  console.log(this.innerText);
+  if (this.innerText == "START CHAT") {
+    document.getElementById("test").style.display = "block";
+    document.getElementById("init").innerText = "CLOSE CHAT";
+    initChat();
+  } else {
+    location.reload();
+  }
+}
+
+function initChat() {
+  j = 0;
+  cbot.innerHTML = "";
+  for (var i = 0; i < len1; i++) {
+    setTimeout(handleChat, i * 500);
+  }
+  setTimeout(function () {
+    showOptions(data.chatinit.options);
+  }, (len1 + 1) * 500);
+}
+
+var j = 0;
+function handleChat() {
+  console.log(j);
+  var elm = document.createElement("p");
+  elm.innerHTML = data.chatinit.title[j];
+  elm.setAttribute("class", "msg");
+  cbot.appendChild(elm);
+  j++;
+  handleScroll();
+}
+
+function showOptions(options) {
+  for (var i = 0; i < options.length; i++) {
+    var opt = document.createElement("span");
+    var inp = "<div>" + options[i] + "</div>";
+    opt.innerHTML = inp;
+    opt.setAttribute("class", "opt");
+    opt.addEventListener("click", handleOpt);
+    cbot.appendChild(opt);
+    handleScroll();
+  }
+}
+
+function handleOpt() {
+  console.log(this);
+  var str = this.innerText;
+  var textArr = str.split(" ");
+  var findText = textArr[0];
+
+  document.querySelectorAll(".opt").forEach((el) => {
+    el.remove();
+  });
+  var elm = document.createElement("p");
+  elm.setAttribute("class", "test");
+  var sp = '<span class="rep">' + this.innerText + "</span>";
+  elm.innerHTML = sp;
+  cbot.appendChild(elm);
+
+  console.log(findText.toLowerCase());
+  var tempObj = data[findText.toLowerCase()];
+  handleResults(tempObj.title, tempObj.options, tempObj.url);
+}
+
+function handleDelay(title) {
+  var elm = document.createElement("p");
+  elm.innerHTML = title;
+  elm.setAttribute("class", "msg");
+  cbot.appendChild(elm);
+}
+
+function handleResults(title, options, url) {
+  for (let i = 0; i < title.length; i++) {
+    setTimeout(function () {
+      handleDelay(title[i]);
+    }, i * 500);
+  }
+
+  const isObjectEmpty = (url) => {
+    return JSON.stringify(url) === "{}";
+  };
+
+  if (isObjectEmpty(url) == true) {
+    console.log("having more options");
+    setTimeout(function () {
+      showOptions(options);
+    }, title.length * 500);
+  } else {
+    console.log("end result");
+    setTimeout(function () {
+      handleOptions(options, url);
+    }, title.length * 500);
+  }
+}
+
+function handleOptions(options, url) {
+  for (var i = 0; i < options.length; i++) {
+    var opt = document.createElement("span");
+    var inp =
+      '<a class="m-link" href="' + url.link[i] + '">' + options[i] + "</a>";
+    opt.innerHTML = inp;
+    opt.setAttribute("class", "opt");
+    cbot.appendChild(opt);
+  }
+  var opt = document.createElement("span");
+  var inp = '<a class="m-link" href="' + url.more + '">' + "See more</a>";
+
+  const isObjectEmpty = (url) => {
+    return JSON.stringify(url) === "{}";
+  };
+
+  console.log(isObjectEmpty(url));
+  console.log(url);
+  opt.innerHTML = inp;
+  opt.setAttribute("class", "opt link");
+  cbot.appendChild(opt);
+  handleScroll();
+}
+
+function handleScroll() {
+  var elem = document.getElementById("chat-box");
+  elem.scrollTop = elem.scrollHeight;
+}
